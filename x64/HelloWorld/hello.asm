bf22e48971c257546822a0860755203c013be9b9
DeonDeakin75328 <127496960+DeonDeakin75328@users.noreply.github.com>
2024-04-23 01:04:46 +1000

    Update hello.asm

--- x64/HelloWorld/hello.asm
+++ x64/HelloWorld/hello.asm
@@ -1,15 +1,19 @@
 ;Hello world.asm
 
 section .data
-    msg     db      "Hello world!",0
+    msg     db      "Hello world",0
 
 section .bss
 
 section .text
     global main
 main:   
-    mov     rax,1
-    mov     rdi,1
-    mov     rsi,msg
-    mov     rdx,12
-    syscall
+    mov rax,1; 
+    mov rdi,1;
+    mov rsi,msg;
+    mov rdx,12;
+    syscall;
+
+    mov rax, 60; clean exit
+    mov rdi, 0;
+    syscall; syscall to invoke clean exit
