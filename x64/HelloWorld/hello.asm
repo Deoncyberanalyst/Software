98100eabc1aa348e4b1d4bae8d757df444cdae66
DeonDeakin75328 <127496960+DeonDeakin75328@users.noreply.github.com>
2024-04-23 01:05:53 +1000

    Update hello.asm
    Added code comments

--- x64/HelloWorld/hello.asm
+++ x64/HelloWorld/hello.asm
@@ -9,10 +9,10 @@ section .text
     global main
 main:   
     mov rax,1; 
-    mov rdi,1;
+    mov rdi,1; 
     mov rsi,msg;
     mov rdx,12;
-    syscall;
+    syscall; syscall to display string
 
     mov rax, 60; clean exit
     mov rdi, 0;
