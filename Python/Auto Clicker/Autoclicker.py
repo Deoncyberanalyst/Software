7e473864b93897c9fc67ddd542541b004eee8702
Deon <117836293+Deoncyberanalyst@users.noreply.github.com>
2025-05-16 18:59:32 +1000

    Improvements to Autoclicker

--- Python/Auto Clicker/Autoclicker.py
+++ Python/Auto Clicker/Autoclicker.py
@@ -1,27 +1,88 @@
 import time
 import pyautogui
 import random
+import sys
+from pynput import mouse, keyboard
+import threading
 
-def click():
-	totalTime = 0 
-	button = 0
-	for i in range(100):
-		sleepTime = random.randrange(30,75)
+stop_event = threading.Event()
+
+def randoMovement(sleepTime):
+	pyautogui.keyDown('right')
+	time.sleep(sleepTime/9)
+	pyautogui.keyUp('right')
+
+def normal_click(sleepTime):
+	pyautogui.leftClick()
+	time.sleep(sleepTime/1000)
+	pyautogui.leftClick()
+
+
+def click(mouseX,mouseY):
+	i=0
+	while not stop_event.is_set() and i < max:
+		pyautogui.moveTo(mouseX,mouseY)
+		sleepTime = random.uniform(1.8,2)
 		print(f"Clicking in: {sleepTime} seconds")
 		time.sleep(sleepTime)
 		
-		button = random.randrange(0,30)
-		
-		if (button==2):
-			pyautogui.typewrite('a')
-			print(f"Button 'a' pressed. Time elapsed: {round(totalTime /60,1)}.\nIterations remaining: {100-i}")
+		randVariation = random.randrange(0,30)
+		if (randVariation==0): 
+			randoMovement(sleepTime)
 		else:
-			pyautogui.leftClick()
-			print(f"Clicked. Time elapsed: {round(totalTime /60,1)}\nIterations remaining: {100-i}\n")
+			i+=1
+			normal_click(sleepTime)
+			print(f"Iterations remaining: {round(max-i)}\n")
+	pyautogui.alert('Script ended')
 
-		totalTime = totalTime + sleepTime
-	
-	pyautogui.alert('Script has ended.\nTotal time elapsed: {round(totalTime/60),2}')
 
-click()
+def get_mouse_pos():
+    print("Press 'x' to set mouse position!")
 
+    mouse_controller = mouse.Controller()
+
+    def on_press(key):
+            if key.char == 'x':
+                pos = mouse_controller.position
+                print(f"Mouse position set to: {pos}")
+                return False  # Stop the listener
+
+    # Start listening to keyboard
+    with keyboard.Listener(on_press=on_press) as listener:
+        listener.join()
+    return mouse_controller.position
+
+def listen_keys():
+	print("Press 'z' to cancel the script.")
+
+	def on_press(key):
+				if key.char == 'z':
+					print("Script cancelled.")
+					stop_event.set() 
+					return False
+
+		# Start listening to keyboard
+	with keyboard.Listener(on_press=on_press) as listener:
+		listener.join()
+
+if __name__ == '__main__':
+	if len(sys.argv) > 1:
+		max = int(sys.argv[1])
+		print(f"Iterations: {max}")
+
+	else:
+		print("No arguments provided.")
+		sys.exit(1)
+
+	thread = threading.Thread(target=listen_keys)
+	mouseX,mouseY = get_mouse_pos()  # Get it first
+	thread2 = threading.Thread(target=click, args=(mouseX,mouseY))
+	thread2.start()
+
+	thread.start()
+
+	if stop_event.is_set():
+		thread.join()
+		thread2.join()
+		print("Script has ended.")
+		sys.exit(0)
